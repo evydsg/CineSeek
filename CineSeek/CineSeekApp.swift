b7f19cd86f9c4f889e44a7034e52245ba575368b
evydsg <118639499+evydsg@users.noreply.github.com>
2024-05-25 15:13:37 -0700

    Initial Commit

---
+++ CineSeek/CineSeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineSeekApp.swift
+//  CineSeek
+//
+//  Created by Evelise Guenda on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CineSeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
