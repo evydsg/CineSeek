ff756f4cd7b7055bf14f8c69ecbf3025eb94278b
evydsg <evelisekguenda@gmail.com>
2024-05-26 11:49:33 -0700

    Day 2

--- CineSeek/Models/Movie.swift
+++ CineSeek/Models/Movie.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct MovieResponse : Decodable {
+    let results : [Movie]
+}
+
+struct Movie : Decodable
+{
+    let id: Int
+    let title : String
+    let backdropPath : String?
+    let posterPath : String?
+    let overview : String
+    let voteAverage : Double
+    let voteCount : Int
+    let runtime : Int?
+}
